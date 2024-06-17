bdc609dc9e3d460827267cad2873475a4c49f451
pawel200020 <pawel200020@gmail.com>
2024-06-17 11:13:32 +0200

    ✨new frontend - next js

---
+++ shop-app-materalize-frontend/src/app/main-page/page.tsx
@@ -0,0 +1,8 @@
+import {Navbar} from "@/app/shared/Navbar";
+
+export default function MainPage(){
+    return (<section id='MainPage' className='main-page-container'>
+        <Navbar/>
+        Hello
+    </section>)
+}
---
+++ shop-app-materalize-frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import MainPage from '@/app/main-page/page';
+
+export default function Home() {
+  return (
+      <main>
+        <MainPage/>
+      </main>
+  );
+}
