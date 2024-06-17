ef7e9cf9376cdbdf8c8bf2ca00ed0240663b1dd7
pawel200020 <pawel200020@gmail.com>
2024-06-17 23:03:41 +0200

    remove feature files from master

--- shop-app-materalize-frontend/src/app/main-page/page.tsx
+++
@@ -1,8 +0,0 @@
-import {Navbar} from "@/app/shared/Navbar";
-
-export default function MainPage(){
-    return (<section id='MainPage' className='main-page-container'>
-        <Navbar/>
-        Hello
-    </section>)
-}
