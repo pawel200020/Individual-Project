63bd65ae984fe8b914bea03c33d72777d5443baa
pawel200020 <pawel200020@gmail.com>
2025-01-07 00:54:53 +0100

    ⬆️next and package upgrades, new navbar layout

--- shop-app-materalize-frontend/src/app/login/page.tsx
+++ shop-app-materalize-frontend/src/app/login/page.tsx
@@ -1,29 +1,36 @@
-import Footer from "@/app/shared/Footer";
+import React from "react";
 
 export default function Login () {
     return(<section id='login' className='main-page-container'>
-        <div className="relative flex flex-col justify-center h-screen overflow-hidden">
-            <div className="w-full p-6 m-auto bg-white rounded-md shadow-md ring-2 ring-gray-800/50 lg:max-w-lg">
-                <h1 className="text-3xl font-semibold text-center text-gray-500">Login</h1>
-                <form className="space-y-4">
-                    <div>
-                        <label className="label">
-                            <span className="text-base label-text">Email</span>
+        <div className="flex justify-center">
+            <div className="card w-96 bg-base-100 shadow-xl mt-20 mb-20">
+                <div className="card-body">
+                    <h2 className="card-title">Login!</h2>
+                    <div className="items-center mt-2">
+                        <label className="input input-bordered flex items-center gap-2 mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                                 className="w-4 h-4 opacity-70">
+                                <path
+                                    d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
+                                <path
+                                    d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
+                            </svg>
+                            <input type="text" className="grow" placeholder="Email"/>
                         </label>
-                        <input type="text" placeholder="Email Address" className="w-full input input-bordered"/>
-                    </div>
-                    <div>
-                        <label className="label">
-                            <span className="text-base label-text">Password</span>
+                        <label className="input input-bordered flex items-center gap-2 mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                                 className="w-4 h-4 opacity-70">
+                                <path fill-rule="evenodd"
+                                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                                      clip-rule="evenodd"/>
+                            </svg>
+                            <input type="password" className="grow" value="password"/>
                         </label>
-                        <input type="password" placeholder="Enter Password" className="w-full input input-bordered"/>
                     </div>
-                    <a href="#" className="text-xs text-gray-600 hover:underline hover:text-blue-600">Forget
-                        Password?</a>
-                    <div>
-                        <button className="btn-neutral btn btn-block">Login</button>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary w-full">Login</button>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </section>)
